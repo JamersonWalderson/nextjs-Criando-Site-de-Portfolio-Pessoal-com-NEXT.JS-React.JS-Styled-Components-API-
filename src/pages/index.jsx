126dbb251aa82768b24a0ccf4cc85631639d1b5e
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-03-23 16:44:49 -0300

    implementation: feita a página home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { FaArrowRight } from 'react-icons/fa'
 import { 
   Container,
   Content,
   Infos,
   Logo,
-  Img
+  Img,
+  Name,
+  Function,
+  Intro,
+  LinkProjects
  } from '../../styles/indexStyles'
 
 export default function Home() {
@@ -12,7 +18,14 @@ export default function Home() {
     <Container>
       <Content>
         <Infos>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum repellendus quis odio, enim assumenda omnis, sapiente reiciendis amet nulla cupiditate explicabo dolore voluptatum deleniti? Non sint nemo fugit corrupti?
+          <Name>Olá, sou Jamerson Walderson</Name>
+          <Function>Fullstack developer</Function>
+          <Intro>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias magnam quibusdam ullam, maxime omnis saepe. Accusamus ad dicta, vel aut praesentium ipsum ducimus, debitis laborum autem aperiam alias at tenetur?
+          </Intro>
+          <Link href='projects'>
+            <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+          </Link>
         </Infos>
         <Logo>
           <Img src="/images/logo.png" alt='logo' />
--- styles/indexStyles.js
+++ styles/indexStyles.js
@@ -1,15 +1,90 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  // background-color: red;
 `
 export const Content = styled.div`
+
+  display: flex;
+  width: 1120px;
+  height: 100%;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+
+  @media(max-width: 600px) {
+    padding: 5px;
+  }
+
+  @media(max-width: 760px) {
+    flex-direction: column-reverse;
+    justify-content: space-around;
+  }
   
 `
 export const Infos = styled.div`
+  display: grid;
+  width: 50%;
+  text-align: start;
+
+  @media(max-width: 760px) {
+    width: 90%;
+  }
   
 `
 export const Logo = styled.div`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 25rem;
+    height: 25rem;
+  }
+
+  @media(max-width: 760px) {
+    width: 90%
+  }
+
+  @media (max-width: 600px) {
+
+    img {
+      width: 18rem;
+      height: 18rem;
+    }
+  }
   
 `
+export const Name = styled.span`
+  font-size: 40px;
+  font-weight: 500;
+
+  @media(max-width: 760px) {
+    font-size: 25px;
+  }
+`
+export const Function = styled.span`
+  color: lightgray;
+  margin-bottom: 10px;
+`
+export const Intro = styled.span``
+
 export const Img = styled.img``
+
+export const LinkProjects = styled.button`
+width: 10rem;
+padding: 10px;
+display: flex;
+justify-content: space-around;
+margin-top: 20px;
+font-weight: bold;
+
+svg {
+  font-size: 20px;
+}
+`
